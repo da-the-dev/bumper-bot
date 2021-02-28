60b979711f4a29e038d6e8c5f856195cba287905
da-the-dev <leshiktk@me.com>
2021-02-28 16:08:22 +0300

    First commit

---
+++ bumpers.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+const dotenv = require('dotenv').config()
+
+// Bumbers
+var bumpers = Array()
+const tokens = [process.env.TOKEN1, process.env.TOKEN2, process.env.TOKEN3]
+
+for(i = 0; i < 3; i++) {
+    /**@type {Discord.Client} */
+    var b = bumpers[i]
+    b = new Discord.Client()
+
+    b.login(tokens[i])
+    b.on('ready', () => {
+        console.log(`Bot bumper${i} ready`)
+    })
+
+    b.on('message', msg => {
+        if(msg.content.includes('пора продвигать сервер в топы! Пропиши') && (msg.author.id == process.env.PINGERID || msg.author.id == process.env.MYID)) {
+            var index = msg.content.indexOf('Пропиши') + 10
+            var command = msg.content.slice(index, msg.content.length - 1)
+            if(command)
+                msg.channel.send(command)
+        }
+    })
+}
